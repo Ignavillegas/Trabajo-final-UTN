8aadfa099f99c5c1372fe9d4cec909284414a9dc
Ignaa <ignaciovillegasdesing@gmail.com>
2023-07-24 23:29:20 -0300

    main

---
+++ src/data.js
@@ -0,0 +1,53 @@
+
+export const data = [
+
+	{
+		id: 1,
+		img: 'https://images.unsplash.com/photo-1570891836654-d4961a7b6929?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1470&q=80',
+		nameProduct: '📱Iphone📱',
+		price: 200.599,
+		quantity: 1,
+	},
+
+	{
+		id: 2,
+		img: 'https://images.unsplash.com/photo-1496181133206-80ce9b88a853?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1471&q=80',
+		nameProduct: '💻MacBook💻',
+		price: 
+		960.899,
+		quantity: 1,
+	},
+
+	{
+		id: 3,
+		img: 'https://images.unsplash.com/photo-1517059224940-d4af9eec41b7?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1305&q=80',
+		nameProduct: '🖥Mac🖥',
+		price: 864.899,
+		quantity: 1,
+	},
+
+	{
+		id: 4,
+		img: 'https://images.unsplash.com/photo-1622979135225-d2ba269cf1ac?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1470&q=80',
+		nameProduct: '👓Watch👓',
+		price: 254.699,
+		quantity: 1,
+	},
+
+	{
+		id: 5,
+		img: 'https://images.unsplash.com/photo-1589739900243-4b52cd9b104e?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=704&q=80',
+		nameProduct: '📱Ipad🖋',
+		price: 343.799,
+		quantity: 1,
+	},
+
+	{
+		id: 6,
+		img: "https://images.unsplash.com/photo-1603539444875-76e7684265f6?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1470&q=80",
+		nameProduct: '🔌USB🔌',
+		price: 38.599,
+		quantity: 1,
+	},
+
+];
